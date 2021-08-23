2a4ee8acf5821a4edbe1f2363f4780ededd45256
Sergey Pluzhnikov <anonimys122@gmail.com>
2021-08-23 21:09:23 +0300

    feat: upload func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) {
+        return Math.abs(n).toString().split('').reverse().join('');
+    }
+    return n.toString().split('').reverse().join('');
 }
